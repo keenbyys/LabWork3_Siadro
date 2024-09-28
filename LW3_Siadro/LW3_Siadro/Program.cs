79fb217ff83eebacf39123d6178a2ae86dfaa779
keenbyys <166899952+keenbyys@users.noreply.github.com>
2024-09-28 11:32:33 +0300

    1.0.010

---
+++ LW3_Siadro/LW3_Siadro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW3_Siadro
+{
+    internal class Program
+    {
+        class Employee
+        {
+            private string name;
+            private string position;
+            private int salary;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+ 
